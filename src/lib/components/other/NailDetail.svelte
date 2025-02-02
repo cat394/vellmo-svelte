4e6a6ff8a4e80d6d94d8f16afbfd562833d63e21
cat394 <keyring1231@gmail.com>
2025-02-03 00:38:11 +0900

    feat: add bg gradient - nail detail

--- src/lib/components/other/NailDetail.svelte
+++ src/lib/components/other/NailDetail.svelte
@@ -7,36 +7,45 @@
 	const nailMenu = menus.find((menu) => menu.category === 'ネイル');
 </script>
 
-<section>
-	<h2>Vell moネイル</h2>
+<section style="position: relative;z-index:0;">
 	<div class="container">
-		<div class="left-side">
-			<StickyNote title="オススメのポイント" headingLevel={3} style="width:fit-content">
-				<ul>
-					<li>初回オフ無料！</li>
-					<li>自店での付け替えオフ無料！</li>
-					<li>完全個室！</li>
-				</ul>
-			</StickyNote>
-			<div class="images">
-				<img src={link('images/nails/room')} alt="Vell moのネイル室" />
-				<img src={link('images/nails/display')} alt="Vell moのネイルの種類" />
-				<img src={link('images/nails/list')} alt="" />
+		<h2>Vell moネイル</h2>
+		<div class="content">
+			<div class="left-side">
+				<StickyNote title="オススメのポイント" headingLevel={3} style="width:fit-content;">
+					<ul>
+						<li>初回オフ無料！</li>
+						<li>自店での付け替えオフ無料！</li>
+						<li>完全個室！</li>
+					</ul>
+				</StickyNote>
+				<div class="images">
+					<img src={link('images/nails/room')} alt="Vell moのネイル室" />
+					<img src={link('images/nails/display')} alt="Vell moのネイルの種類" />
+					<img src={link('images/nails/list')} alt="" />
+				</div>
 			</div>
-		</div>
-		<div class="right-side">
-			{#if nailMenu}
-				<MenuTable title="Menu" plans={nailMenu.plans} repletion />
-			{:else}
-				<p>ネイルのデータが見つかりませんでした。</p>
-			{/if}
+			<div class="right-side">
+				{#if nailMenu}
+					<MenuTable title="Menu" plans={nailMenu.plans} repletion />
+				{:else}
+					<p>ネイルのデータが見つかりませんでした。</p>
+				{/if}
+			</div>
 		</div>
 	</div>
 </section>
 
 <style>
+	section {
+		background-image:  repeating-linear-gradient(135deg, transparent, transparent 18px, rgba(166, 107, 107, 1) 18px, rgba(166, 107, 107, 1) 28px );
+		padding: 1.5rem;
+	}
+
 	h2 {
-		font-size: 1.5rem;
+		font-size: 2rem;
+		text-align: center;
+		margin-bottom: 2rem;
 	}
 
 	ul {
@@ -50,9 +59,18 @@
 	}
 
 	.container {
+		background: var(--bg-primary);
+		padding: 1rem;
+	}
+
+	.content {
 		display: grid;
 		grid-template-columns: 1fr 1fr;
 		gap: 3rem 1rem;
+	}
+
+	.left-side {
+		z-index: 1;
 	}
 
 	.right-side {
@@ -66,7 +84,7 @@
 	}
 
 	@media (width < 965px) {
-		.container {
+		.content {
 			grid-template-columns: 1fr;
 		}
 	}
