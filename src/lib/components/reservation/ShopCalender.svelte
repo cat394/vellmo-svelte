e86bc1af6d699385ad7d8483bfbeb64bcd13b2e0
cat394 <keyring1231@gmail.com>
2025-02-08 07:34:22 +0900

    feat: add calender detail description

--- src/lib/components/reservation/ShopCalender.svelte
+++ src/lib/components/reservation/ShopCalender.svelte
@@ -17,7 +17,7 @@
 		希望する時間帯が空いているかどうかを調べるためには、<a
 			href="https://calendar.google.com/calendar/embed?src=326855ca45ebc37d17ab7612fe61cd17e883197d895a60f5555323f957fc9366%40group.calendar.google.com&ctz=Asia%2FTokyo"
 			target="_blank">当店の公開カレンダー</a
-		>をご覧ください。予約が入っている枠は『予定あり』と書かれています。
+		>をご覧ください。<br />予約が入っている枠は『予定あり』と書かれています。<br />カレンダーの右上にある格子のアイコンから月の表示を週に切り替えたりと、見やすく表示を切り替えることができます。
 	</p>
 </section>
 
