89fc00cc4d8946a47eb28d4c3bae91ab4f41b23a
cat394 <keyring1231@gmail.com>
2025-08-15 14:47:03 +0900

    営業時間を「毎週月曜日、第2・第4の金曜日」から「9:30～18:00」に修正

--- src/lib/data/shop.ts
+++ src/lib/data/shop.ts
@@ -3,7 +3,7 @@ export const shop = {
 	name: 'Style & Color Vell mo',
 	tel: '0297-85-2407',
 	address: '〒300-1535 茨城県取手市清水1112',
-	opening: '毎週月曜日、第2・第4の金曜日',
+	opening: '9:30～18:00',
 	closed: '毎週月曜日、第2・第4の金曜日',
 	reservationForm: 'https://forms.gle/BisyQdsRwvfhDxuF7',
 	contactForm: 'https://forms.gle/6ugLPkGF2EU8iJjC8'
