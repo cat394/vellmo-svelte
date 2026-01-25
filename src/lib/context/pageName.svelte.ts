0720b62982eb2c2c98bcb8fdb1e6b4239f225cf9
cat394 <keyring1231@gmail.com>
2026-01-25 12:02:59 +0900

    remove unnecessary page contents

--- src/lib/context/pageName.svelte.ts
+++ src/lib/context/pageName.svelte.ts
@@ -5,10 +5,8 @@ export enum PageNames {
 	about = '当店について',
 	price = '当店のメニュー',
 	products = '商品ページ',
-	reservation = '予約ページ',
 	others = 'その他サービス',
 	contact = 'お問い合わせ',
-	site = '当サイトについて'
 }
 
 class PageNameContext {
